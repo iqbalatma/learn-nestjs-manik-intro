78af96902f79c1d7031376a1aad7b5feca9b22df
iqbalatma <iqbalatma@gmail.com>
2024-08-18 15:15:55 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 
 @Module({
@@ -14,12 +15,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [],
       useFactory: () => ({
           type: "postgres",
-          entities: [],
+          entities: [User],
           synchronize: true,
           host: "localhost",
           port: 5432,
+          username: "iqbalatma",
           password: "lnfavjem",
-          database: "nest"
+          database: "nestjs"
       }),
     })
   ],
